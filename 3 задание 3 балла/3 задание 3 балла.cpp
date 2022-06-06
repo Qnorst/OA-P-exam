654dfdee69bbf9469c98ec86077a3fa4325d967b
Владимир Денисенко <vladimir090703@mail.ru>
2022-06-07 06:32:06 +0700

    now it can be 4

---
+++ 3 задание 3 балла/3 задание 3 балла.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int size = 10;
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << "arr[" << i << "] = ";
+        std::cin >> arr[i];
+    }
+    std::cout << "Your array: ";
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << '\t';
+    }
+    std::cout << std::endl;
+
+    int min = INT_MAX;
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    int mult = 1;
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] - min == 2)
+        {
+            mult *= arr[i];
+        }
+    }
+    std::cout << "Multiply of nums differ from min by 2 = " << mult;
+    delete[] arr;
+}
